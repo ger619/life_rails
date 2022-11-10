899ee1f5ebaf55924cbc95af330022c3e9ee92e5
David Abol Ger <draker74@gmail.com>
2022-11-10 13:02:38 +0300

    Add biodates

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  resources :users, only: [:index]
+  resources :users, only: [:index, :show]
 end
