1b4713685dec082e7506a0c8c1826a9a1045a9a5
David Abol Ger <draker74@gmail.com>
2022-11-11 17:13:10 +0300

    Add all data

--- test/controllers/bio_controller_test.rb
+++ test/controllers/bio_controller_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BioControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
--- test/models/bio_test.rb
+++ test/models/bio_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class BioTest < ActiveSupport::TestCase
   # test "the truth" do
